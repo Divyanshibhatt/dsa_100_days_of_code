5bd71bab0a07333523ad94528a625200c59d70dc
Divyanshibhatt <divyanshib2@gmail.com>
2026-02-14 21:29:08 +0530

    day 14 question

---
+++ day14/checvk_identity_metrix.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, j;
+    int isIdentity = 1;
+
+    printf("Enter size of square matrix: ");
+    scanf("%d", &n);
+
+    int matrix[n][n];
+
+    printf("Enter matrix elements:\n");
+    for(i = 0; i < n; i++)
+    {
+        for(j = 0; j < n; j++)
+        {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    // Check identity condition
+    for(i = 0; i < n; i++)
+    {
+        for(j = 0; j < n; j++)
+        {
+            if(i == j)
+            {
+                if(matrix[i][j] != 1)
+                {
+                    isIdentity = 0;
+                    break;
+                }
+            }
+            else
+            {
+                if(matrix[i][j] != 0)
+                {
+                    isIdentity = 0;
+                    break;
+                }
+            }
+        }
+        if(isIdentity == 0)
+            break;
+    }
+
+    if(isIdentity == 1)
+        printf("Matrix is an Identity Matrix.\n");
+    else
+        printf("Matrix is NOT an Identity Matrix.\n");
+
+    return 0;
+}
